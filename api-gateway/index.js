4852122d8dfa7e49c2521d5afd73f88c1455512f
nicholas-gcc <nicholasgcc@gmail.com>
2023-03-06 12:47:03 +0800

    Define api-gateway and Dockerfiles for auth microservice and api-gateway

---
+++ api-gateway/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer();
+const app = express();
+
+// Route requests to the auth service
+app.use('/auth', (req, res) => {
+  proxy.web(req, res, { target: 'http://auth:3000' });
+});
+
+// Route requests to the order service
+app.use('/orders', (req, res) => {
+  proxy.web(req, res, { target: 'http://order:3001' });
+});
+
+// Route requests to the product service
+app.use('/products', (req, res) => {
+  proxy.web(req, res, { target: 'http://product:3002' });
+});
+
+// Start the server
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`API Gateway listening on port ${port}`);
+});
---
+++ auth/Dockerfile
@@ -0,0 +1,17 @@
+# Use an official Node.js runtime as a parent image
+FROM node:14
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container
+COPY package*.json ./
+
+# Install app dependencies
+RUN npm install
+
+# Copy the rest of the application files to the container
+COPY . .
+
+# Set the command to start the app
+CMD [ "npm", "start" ]
